6c05bed39e74af1de6d4a05044c7a03100cebd86
Charmi Kansara <charmi18kansara@gmail.com>
2021-07-20 21:16:35 +0530

    Transpose of 3*3 matrix

---
+++ MatrixTranspose.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func Transpose(a [][]int) {
+	var trans [3][3]int
+	//Transpose operation
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a[0]); j++ {
+			trans[i][j] = a[j][i]
+		}
+	}
+	//Print transpose of matrix
+	for i := 0; i < len(trans); i++ {
+		for j := 0; j < len(trans[0]); j++ {
+			fmt.Print(trans[i][j])
+		}
+		fmt.Println("")
+	}
+
+}
+func main() {
+	a := [][]int{
+		[]int{1, 2, 3},
+		[]int{4, 5, 6},
+		[]int{7, 8, 9},
+	}
+	Transpose(a)
+
+}
